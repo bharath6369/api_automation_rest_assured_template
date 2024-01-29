f666b7f8f4a87713f039289b38920e44fad09a27
bharath6369 <panyalabharath.reddy@walkingtree.tech>
2024-01-29 18:14:14 +0530

    constant changes

--- src/test/java/token/Constant.java
+++ src/test/java/token/Constant.java
@@ -7,8 +7,6 @@ public class Constant {
 
     public static String END_POINT_TOKEN_GENERATE="/api/auth/auth/oauth/token";
     public static String END_POINT_USER_INFO="/api/auth/auth/user";
-    public static String END_POINT_CREATE_OPERATOR="/api/operator/v1/operators/";
-    public static String END_POINT_GET_OPERATOR_BY_ID="/api/operator/v1/operators/{operatorId}";
 
 
 }
