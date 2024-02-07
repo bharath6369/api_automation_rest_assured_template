17e87750b35db61b03801fa046a3a6c948ec42d2
Bharath <109291386+bharath6369@users.noreply.github.com>
2024-02-07 13:20:35 +0530

    Update Constant.java

--- src/test/java/token/Constant.java
+++ src/test/java/token/Constant.java
@@ -2,6 +2,7 @@ package token;
 
 public class Constant {
     public static String BASE_URL="https://test-test.test.ai";
+    public static String END_POINT_TOKEN_GENERATE = "/api/auth/auth/oauth/token";
 
 
 
